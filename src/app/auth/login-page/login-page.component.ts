dbd28059a1d3355d4cc3b16bee86474983a56b65
Josh Mack <mack8.jo@gmail.com>
2023-10-29 17:50:34 -0400

    feat(auth): add login form

--- src/app/auth/login-page/login-page.component.ts
+++ src/app/auth/login-page/login-page.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'joshies-login-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, InputTextModule, ButtonModule, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent {
-
+  readonly form = new FormGroup({});
+  onSubmit(event: Event): void {
+    event.preventDefault();
+    alert('Login successful');
+  }
 }
