6479aa65b452f3ea9a0c25bfadc6934039580085
Josh Mack <mack8.jo@gmail.com>
2023-10-29 16:57:56 -0400

    feat(auth): add barebones login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './auth/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+];
---
+++ src/app/auth/login-page/login-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'joshies-login-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent {
+
+}
