f8e83316f3d2c4f1b0f4a70310f180038a787add
Colin Williams <colin@colinaw.com>
2024-04-20 21:23:46 -0400

    feat(card): adjust card link size, add options for pretext and subtext

--- src/app/shared/ui/card-link.component.ts
+++ src/app/shared/ui/card-link.component.ts
@@ -5,33 +5,47 @@ import {
   booleanAttribute,
   input,
 } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 export interface CardLinkModel {
   iconClass: string;
   text: string;
+  subtext?: string;
+  pretext?: string;
   routerLink: string | unknown[];
 }
 
 @Component({
   selector: 'joshies-card-link',
   standalone: true,
-  imports: [RouterLink, NgClass],
+  imports: [RouterLink, RouterLinkActive, NgClass],
   template: `
     <a
-      class="flex w-full no-underline text-color"
+      class="flex w-full no-underline text-color transition-colors transition-duration-100 transition-ease-in-out"
       [routerLink]="model().routerLink"
     >
       <i
-        class="text-white mx-3 my-2 p-1 border-round-sm align-self-center"
+        class="h-2rem w-2rem flex-shrink-0 text-lg text-white mx-3 my-2 flex justify-content-center align-items-center align-self-center border-round-md"
         [ngClass]="model().iconClass"
       ></i>
       <div
         class="flex align-items-center w-full justify-content-between pl-0 p-2 border-100"
         [class.border-bottom-1]="!last()"
       >
-        {{ model().text }}
-        <i class="pi pi-angle-right text-300"></i>
+        @if (model().subtext || model().pretext) {
+          <div>
+            @if (model().pretext) {
+              <p class="m-0 text-400 text-sm">{{ model().pretext }}</p>
+            }
+            <p class="m-0">{{ model().text }}</p>
+            @if (model().subtext) {
+              <p class="m-0 text-400 text-sm">{{ model().subtext }}</p>
+            }
+          </div>
+        } @else {
+          {{ model().text }}
+        }
+        <i class="pi pi-angle-right text-300 ml-3"></i>
       </div>
     </a>
   `,
