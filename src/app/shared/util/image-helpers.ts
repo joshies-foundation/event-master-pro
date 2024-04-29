08b9ab4c27c86c49653c9ab2a7faceefe1829c78
Colin Williams <colin@colinaw.com>
2024-04-29 17:59:56 -0400

    fix(profile): crop image properly so avatar is not squished

--- src/app/shared/util/image-helpers.ts
+++ src/app/shared/util/image-helpers.ts
@@ -12,7 +12,34 @@ export function resizeImage(
       canvas.height = height ?? width;
       const canvasRenderingContext = canvas.getContext('2d');
       if (canvasRenderingContext) {
-        canvasRenderingContext.drawImage(image, 0, 0, width, height ?? width);
+        // Calculate cropping area to maintain aspect ratio
+        let cropX = 0;
+        let cropY = 0;
+        let cropWidth = image.width;
+        let cropHeight = image.height;
+        const aspectRatio = image.width / image.height;
+        const targetAspectRatio = canvas.width / canvas.height;
+
+        if (aspectRatio > targetAspectRatio) {
+          // Crop the width
+          cropWidth = image.height * targetAspectRatio;
+          cropX = (image.width - cropWidth) / 2;
+        } else {
+          // Crop the height
+          cropHeight = image.width / targetAspectRatio;
+          cropY = (image.height - cropHeight) / 2;
+        }
+        canvasRenderingContext.drawImage(
+          image,
+          cropX,
+          cropY,
+          cropWidth,
+          cropHeight,
+          0,
+          0,
+          width,
+          height ?? width,
+        );
       }
       canvas.toBlob((blob) => resolve(blob as Blob), 'image/webp');
     };
