5971015f3e1f000a7fa20f734dcf4b37259e9d2c
Josh <mack8.jo@gmail.com>
2024-05-16 23:17:13 -0400

    style(events): add border shadow to event order drag/drop

--- src/app/gm-tools/feature/manage-events-page.component.ts
+++ src/app/gm-tools/feature/manage-events-page.component.ts
@@ -144,6 +144,11 @@ import { PlayerService } from '../../shared/data-access/player.service';
     .cdk-drag-preview {
       border: none;
       background-color: var(--surface-card);
+      border-radius: 4px;
+      box-shadow:
+        0 5px 5px -3px rgba(0, 0, 0, 0.2),
+        0 8px 10px 1px rgba(0, 0, 0, 0.14),
+        0 3px 14px 2px rgba(0, 0, 0, 0.12);
     }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
