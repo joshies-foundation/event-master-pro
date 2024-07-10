65880826d86e4ea2c9bb15ebbbe7fef4d1383bfe
Colin Williams <40863449+c-o-l-i-n@users.noreply.github.com>
2024-07-09 20:47:40 -0400

    feat(db): add prod db config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
-const supabaseUrl = 'https://hqomdxggwvkmaovkytld.supabase.co';
+const supabaseUrl = 'https://bbptbpmmfformxfzkdgl.supabase.co';
 const supabasePublicStorageUrl = supabaseUrl + '/storage/v1/public/';
 
 export const environment = {
   supabase: {
     url: supabaseUrl,
     publicStorageUrl: supabasePublicStorageUrl,
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhxb21keGdnd3ZrbWFvdmt5dGxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDczNDk0NDQsImV4cCI6MjAyMjkyNTQ0NH0.tMpDQroK0_zOGlfjZdxVap9Q_vnnZssaWXy06sMv8vo',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJicHRicG1tZmZvcm14ZnprZGdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA1NjkyOTIsImV4cCI6MjAzNjE0NTI5Mn0.Sg9N3RiwNARaxO9xt6RDY7Ksug_sEkYcIOcBKhJnAZU',
   },
   vapidPublicKey:
     'BH1snV0pt9ZbfWNxKvMjkuh1vELvWjg5VaFxw5ITY0eS1bG86ZdQp7wmlfQU8MjXmPftj7qtUBExEhHEqm2VgaM',
