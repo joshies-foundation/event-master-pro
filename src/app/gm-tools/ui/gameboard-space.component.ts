b687a7f081585281c421c0fa78b0893f8c42ec1d
Colin Williams <40863449+c-o-l-i-n@users.noreply.github.com>
2025-05-03 12:51:38 -0400

    refactor(gameboard): use `host` component metadata field instead of `@HostBinding` for gameboard space

--- src/app/gm-tools/ui/gameboard-space.component.ts
+++ src/app/gm-tools/ui/gameboard-space.component.ts
@@ -1,9 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  HostBinding,
-  input,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 interface GameboardSpaceVisualizationModel {
   icon_class: string | null;
@@ -14,16 +9,13 @@ interface GameboardSpaceVisualizationModel {
   selector: 'joshies-gameboard-space',
   imports: [],
   template: '',
+  host: {
+    '[class]':
+      "model().icon_class + ' size-8 self-start shrink-0 rounded-full border-2 border-gray-300 text-lg text-gray-300 flex! justify-center items-center'",
+    '[style.background]': 'model().color',
+  },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GameboardSpaceComponent {
   model = input.required<GameboardSpaceVisualizationModel>();
-
-  @HostBinding('class') get hostClass(): string {
-    return `${this.model().icon_class} size-8 self-start shrink-0 rounded-full border-2 border-gray-300 text-lg text-gray-300 flex! justify-center items-center`;
-  }
-
-  @HostBinding('style') get hostStyle(): string {
-    return `background: ${this.model().color}`;
-  }
 }
